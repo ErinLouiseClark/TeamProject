1cccd7360c0a862650d62ae7ed0a57ce817fb076
erin <erin@192.168.1.28>
2016-11-14 20:00:00 -0700

    Erin Homewor 11/14/16

--- SISTeamProject/src/TeamProjectRunner.java
+++ SISTeamProject/src/TeamProjectRunner.java
@@ -5,5 +5,13 @@ public class TeamProjectRunner {
 		
 
 	}
+	
+	public void welcomeUser(){
+		System.out.println("Welcome to the class roster!");
+		System.out.println("What would you like to do?"
+				+ "1) add or delete a student"
+				+ "2) change a student's grade or schedule"
+				+ "3) sort students");
+	}
 
 }
