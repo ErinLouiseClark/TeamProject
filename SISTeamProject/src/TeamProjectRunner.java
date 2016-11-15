a80eaf988b2a1174c46b75b0544914ef696dc359
Sizhuang Li <strongli2014@hotmail.com>
2016-11-15 14:44:30 -0700

    Add all content

--- SISTeamProject/src/TeamProjectRunner.java
+++ SISTeamProject/src/TeamProjectRunner.java
@@ -2,16 +2,17 @@
 public class TeamProjectRunner {
 
 	public static void main(String[] args) {
+		welcomeUser();
 		
-
 	}
 	
-	public void welcomeUser(){
+	public static void welcomeUser(){
 		System.out.println("Welcome to the class roster!");
-		System.out.println("What would you like to do?"
-				+ "1) add or delete a student"
-				+ "2) change a student's grade or schedule"
-				+ "3) sort students");
-	}
+		System.out.println("What would you like to do?");
+		System.out.println("1) add or delete a student");
+		System.out.println( "2) change a student's grade or schedule");
+		System.out.println("3) sort students");
+	}//basically, make welcomeUser() static.
+	//make three choices in seperate lines.
 
 }
