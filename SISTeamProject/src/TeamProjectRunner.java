f5cb4fd373e1850fe9b28cc6c31093e8bb69c91c
George Zhu <zyqgeorge@gmail.com>
2016-11-17 13:54:20 -0700

    test

--- SISTeamProject/src/TeamProjectRunner.java
+++ SISTeamProject/src/TeamProjectRunner.java
@@ -8,7 +8,7 @@ public class TeamProjectRunner {
 	
 	public static void welcomeUser(){
 		System.out.println("Welcome to the class roster!");
-		System.out.println("What would you like to do?");
+		System.out.println("What would you like to do?"
 				+ "1) add or delete a student"
 				+ "2) change a student's grade or schedule"
 				+ "3) sort students");
@@ -21,6 +21,26 @@ public class TeamProjectRunner {
 			
 		}
 		else if(task == 3){
+			
+		}
+		else{
+			System.out.println("That is an invalid option! Try again.");
+			welcomeUser();
+		}
+	}
+	
+	public static void changeGrade(){
+		System.out.println("1) Change Grade");
+		System.out.println("2) Switch Classes");
+		Scanner userInput = new Scanner(System.in);
+		int choice = userInput.nextInt();
+        if(choice == 1){
+			 
+		}
+		else if(choice == 2){
+			
+		}
+		else if(choice == 3){
 			
 		}
 		else{
